dded71029d5531d567457c0e2b67d6f4d1f4a159
nickcube2 <awuninicholas@gmail.com>
2022-04-09 09:59:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,16 @@
-#!/bin/bash
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
-printf("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-return (1);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
 }
