98490988b0139b86af11d5997e0e8ea8b81eeb07
nickcube2 <awuninicholas@gmail.com>
2022-04-09 08:46:50 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alpabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
